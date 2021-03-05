5f6216a134eae179e8db687c62ed5fcba89a0d88
Ivy <meivy@Ivys-MBP.home>
2021-03-04 16:11:00 -0800

    added prototype 1

---
+++ des157/final/proto/script.js
@@ -0,0 +1,50 @@
+(function(){
+    "use strict";
+    console.log("reading js");
+
+    window.addEventListener('load', function () {
+        'use strict'
+    
+        /* variables used to make up the image slider */
+        let sliderContent;
+        let sliderWidth;
+        let fullSlider = document.querySelector('body');
+        const clonedSlider = fullSlider.innerHTML;
+    
+        /* This is the slider animation function  */
+        function animateSlider() {
+            sliderContent = document.querySelector('.a');
+            sliderWidth = sliderContent.offsetWidth;
+            const cloned = sliderContent.cloneNode(true);
+            cloned.className = "b";
+            document.querySelector('#slider').appendChild(cloned);
+            document.querySelector('#slider').style.left = `-${sliderWidth}px`;
+            repeatAnimation();
+        }
+    
+        /* This function repeats the slider motion going from left to right */
+        animateSlider();
+        function repeatAnimation() {
+            fullSlider.addEventListener("transitionend", function () {
+                console.log('going around again!');
+                document.querySelector('body').innerHTML = clonedSlider;
+                fullSlider = document.querySelector('#slider');
+                animateSlider();
+            });
+        }
+    });
+
+
+/* TRIAL CODES BELOW/ IGNORE */
+
+   /*  document.getElementById("one").addEventListener("click", function(){
+        document.getElementById("italy");
+        <a href="trial.html"></a>
+    }); */
+
+    /* function imageClick(url) {
+        window.location = url;
+    }
+    imageClick(); */
+
+}());
